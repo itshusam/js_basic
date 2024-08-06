d3bd0fc764bffb8feef085fcd9bc278476fc31f9
itshusam <husam.attarabeen@gmail.com>
2024-08-06 04:11:31 -0500

    first commit

---
+++ 1.js
@@ -0,0 +1,18 @@
+
+let studentName;
+let studentAge;
+let studentGrade;
+let isPresent;
+
+       
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+
+      
+console.log("Student Name: " + studentName);
+console.log("Student Age: " + studentAge);
+console.log("Student Grade: " + studentGrade);
+console.log("Is Present: " + isPresent);
+        
